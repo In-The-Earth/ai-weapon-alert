18cd6fd2bd63bb3bd1ca50608a2f514da9bff4f4
Natchanon <natchanon.earth1505@gmail.com>
2022-03-29 16:34:31 +0700

    final_test

--- service/ai_cam_service.py
+++ service/ai_cam_service.py
@@ -6,4 +6,4 @@ parser.add_argument('--id', type=str, required=True)
 parser.add_argument('--url', type=str, required=True)
 args = parser.parse_args()
 
-run(weights='weapon/best_sohas.pt',conf_thres=0.6,source=args.url,nosave=True,idCamera=args.id)
+run(weights='weapon/sohas60hk.pt',conf_thres=0.6,source=args.url,nosave=True,idCamera=args.id)
